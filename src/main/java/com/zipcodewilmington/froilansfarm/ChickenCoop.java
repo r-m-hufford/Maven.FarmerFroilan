379d3f920a05cc0e0f37340b19a725a5022f6c94
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-03-25 23:24:58 -0400

    added chickens to the chickencoop, added name to farmer

--- src/main/java/com/zipcodewilmington/froilansfarm/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ChickenCoop.java
@@ -5,4 +5,20 @@ import java.util.List;
 
 public class ChickenCoop {
     List<Chicken> chickens = new ArrayList<Chicken>();
+
+
+
+    public ChickenCoop(Chicken ...chickens){
+        for(Chicken chicken : chickens){
+            this.chickens.add(chicken);
+        }
+    }
+
+    public List<Chicken> getChickens() {
+        return chickens;
+    }
+
+    public void setChickens(List<Chicken> chickens) {
+        this.chickens = chickens;
+    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
@@ -2,7 +2,19 @@ package com.zipcodewilmington.froilansfarm;
 
 import com.zipcodewilmington.froilansfarm.edible.Edible;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Farmer extends Person implements Botanist, Rider, Pilot{
+    private String name;
+
+
+
+    public Farmer (String name) {
+        this.name = name;
+
+    }
+
     public void plantCrop() {
 
     }
@@ -22,4 +34,8 @@ public class Farmer extends Person implements Botanist, Rider, Pilot{
     public void dismount(Rideable rideable) {
 
     }
+
+
+
+
 }
