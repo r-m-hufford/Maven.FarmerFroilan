8afe557b5af36024e0c554b2c14fb7f0136dd1bf
Theresa <theresa@Gbrads-Mac.home>
2021-03-26 00:32:26 -0400

    :(

--- src/test/java/com/zipcodewilmington/froilansfarm/FarmerTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmerTest.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.froilansfarm;
 
 import com.zipcodewilmington.froilansfarm.humans.Farmer;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,13 +11,14 @@ public class FarmerTest {
     @Test
     public void farmerConstructorTest() {
         // Given
-        String name = "Froilanda";
+        String name = "Froilan";
 
         // When
         Farmer farmer = new Farmer(name);
+        String actual = farmer.getName();
 
         // Then
-        //Assert.
+        Assert.assertEquals(name, actual);
 
     }
 
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/TuesdayTest.java
@@ -0,0 +1,20 @@
+package com.zipcodewilmington.froilansfarm;
+
+import com.zipcodewilmington.froilansfarm.humans.Farmer;
+import org.junit.Test;
+
+public class TuesdayTest {
+
+    @Test
+    public void testTuesday(){
+        // Given
+        String name = "Froilan";
+        Farmer farmer = new Farmer("Froilan");
+
+
+        // When
+
+        // Then
+    }
+
+}
