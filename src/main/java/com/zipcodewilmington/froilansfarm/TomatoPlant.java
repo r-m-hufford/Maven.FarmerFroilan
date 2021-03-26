0e371ccb2e96bab017acfbbc067644481515286c
r-m-hufford <ryan@rmhufford.com>
2021-03-25 22:50:20 -0400

    tomato plant

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/TomatoPlant.java
@@ -0,0 +1,9 @@
+package com.zipcodewilmington.froilansfarm;
+
+import com.zipcodewilmington.froilansfarm.edible.Edible;
+
+public class TomatoPlant extends Crop{
+    public void yield(Edible edible) {
+
+    }
+}
