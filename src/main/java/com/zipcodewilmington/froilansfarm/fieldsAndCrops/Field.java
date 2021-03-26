a15fe6dffe212172d8fe6ee4c19f6f51fccdec00
r-m-hufford <ryan@rmhufford.com>
2021-03-26 00:22:19 -0400

    field

--- src/main/java/com/zipcodewilmington/froilansfarm/fieldsAndCrops/Field.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/fieldsAndCrops/Field.java
@@ -1,4 +1,10 @@
 package com.zipcodewilmington.froilansfarm.fieldsAndCrops;
 
+import com.zipcodewilmington.froilansfarm.vehicles.FarmVehicle;
+
+import java.util.List;
+
 public class Field {
+
+    List<FarmVehicle>
 }
