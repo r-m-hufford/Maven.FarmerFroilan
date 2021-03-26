986b50525d7ed3fc1be422cec48cdf94171de08b
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-03-25 22:49:16 -0400

    added Farmer class that extneds Person and implements pilot, rider, botanist

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
@@ -0,0 +1,25 @@
+package com.zipcodewilmington.froilansfarm;
+
+import com.zipcodewilmington.froilansfarm.edible.Edible;
+
+public class Farmer extends Person implements Botanist, Rider, Pilot{
+    public void plantCrop() {
+
+    }
+
+    public void eat(Edible edible) {
+
+    }
+
+    public String makeNoise() {
+        return null;
+    }
+
+    public void mount(Rideable rideable) {
+
+    }
+
+    public void dismount(Rideable rideable) {
+
+    }
+}
